439c5a87bb704c3ece04f710b072166db23ecfdd
SoftWareEngeneerDev <mailforlearning19@gmail.com>
2022-09-02 14:39:16 +0000

    Fin de l'exercice2 Week2 Day1  DailyChallenge

---
+++ week2/Day1/DailyChallenge/Exercice2.js
@@ -0,0 +1,13 @@
+/*
+
+------------------------------------------Exercise 2---------------------------------
+Using this array :
+
+let moreFruits = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+Access and then console.log “Oranges”.
+*/
+
+
+let moreFruits = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+//Access and then console.log “Oranges”.
+console.log(moreFruits[1][1][0])
