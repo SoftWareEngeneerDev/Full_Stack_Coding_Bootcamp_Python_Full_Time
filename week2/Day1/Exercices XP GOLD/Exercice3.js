feec77cdea90af720ba95986fdfab2ac0aac2af7
SoftWareEngeneerDev <mailforlearning19@gmail.com>
2022-09-02 14:49:24 +0000

    fin de l'exercice3 Week2 Day1 Exercice XP GOLD

---
+++ week2/Day1/Exercices XP GOLD/Exercice3.js
@@ -0,0 +1,4 @@
+let num1 = prompt('Entez le 1er nombre SVP!:', 20);
+let num2 = prompt('Entrez le 2e nombre SVP!:', 20);
+let num1 += num2;
+alert(` La somme des deux nombre que vous avez renseiller est:${somme}`)
