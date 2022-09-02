1fcb0ea17002a81f5fe76ecbe9c830af8aeaeab7
SoftWareEngeneerDev <mailforlearning19@gmail.com>
2022-09-02 14:43:50 +0000

    fin de l'exercice1 Week2 Day1 Exercice XP

---
+++ week2/Day1/Exercices XP/Exercices1.js
@@ -0,0 +1,12 @@
+/*
+Store your favorite food into a variable.
+
+Store your favorite meal of the day into a variable (ie. breakfast, lunch or dinner)
+
+Console.log I eat <favorite food> at every <favorite meal>
+
+*/
+//-------------------Resolution-----------------------------
+let favorite_food = 'crudité';
+let dinner = 'Soupé';
+console.log("I eat" + favorite_food + "every " + dinner);
