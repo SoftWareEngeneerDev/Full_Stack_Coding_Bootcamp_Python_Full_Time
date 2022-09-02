b8325ef5f64d94cf3c4e9304b5f1ddb3f5d72fba
SoftWareEngeneerDev <mailforlearning19@gmail.com>
2022-09-02 14:27:31 +0000

    Fin de l'exercice1 Day2

---
+++ week2/Day2/Exercices XP/Exercice1.js
@@ -0,0 +1,23 @@
+/*
+-------------------------------------------Exercise 1---------------------------------- 
+Simple If/Else Statement
+Instructions
+Create 2 variables, x and y. Each of them should have a different numeric value.
+Write an if/else statement that checks which number is bigger.
+Example :
+
+let x = 5;
+let y = 2;
+
+You should display:
+x is the biggest number
+*/
+
+let x = 7;
+let y = 9;
+
+if (x > y) {
+    console.log(x + " " + "Is the biggest number")
+} else {
+    console.log(y + " " + "Is the biggest number")
+}
