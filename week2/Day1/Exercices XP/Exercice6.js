0294191baaa5fd8c20950ff36b58cd0e68feabc8
SoftWareEngeneerDev <mailforlearning19@gmail.com>
2022-09-02 14:45:40 +0000

    fin de l'exercice6 Week2 Day1 Exercice XP

---
+++ week2/Day1/Exercices XP/Exercice6.js
@@ -0,0 +1,55 @@
+5 + "34"
+    // Prediction:439
+    // Actual:'439'
+
+5 - "4"
+    // Prediction:1
+    // Actual:1
+
+10 % 5
+    // Prediction:0
+    // Actual:0
+
+5 % 10
+    // Prediction:0
+    // Actual:5
+
+    "Java" + "Script"
+    // Prediction: 'JavaScript'
+    // Actual:
+
+" " + " "
+// Prediction:' '
+// Actual:' '
+
+" " + 0
+// Prediction:'0'
+// Actual:' 0'
+
+true + true
+    // Prediction:2
+    // Actual:2
+
+true + false
+    // Prediction:1
+    // Actual:
+
+false + true
+    // Prediction:1
+    // Actual:1
+
+false - true
+    // Prediction:-1
+    // Actual:
+
+    !true
+    // Prediction:false
+    // Actual:false
+
+3 - 4
+    // Prediction:-1
+    // Actual:-1
+
+    "Bob" - "bill"
+    // Prediction:NaN
+    // Actual:NaN
