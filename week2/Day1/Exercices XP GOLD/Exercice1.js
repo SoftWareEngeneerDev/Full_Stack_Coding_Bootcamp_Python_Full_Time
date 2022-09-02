afae74240cb54e364df7962da592e22c1cc77f3f
SoftWareEngeneerDev <mailforlearning19@gmail.com>
2022-09-02 14:47:52 +0000

    fin de l'exercice1 Week2 Day1 Exercice XP GOLD

---
+++ week2/Day1/Exercices XP GOLD/Exercice1.js
@@ -0,0 +1,9 @@
+/*
+-------------------------------Instructions------------------------------------------------------------------
+let sentence = ["my","favorite","color","is","blue"];
+Write some simple Javascript code that will join all the items in the array above, and console.log the result.
+
+*/
+let sentence = ["my", "favorite", "color", "is", "blue"];
+let text = sentence.join();
+console.log(text)
