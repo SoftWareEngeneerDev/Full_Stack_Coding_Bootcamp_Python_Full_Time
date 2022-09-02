37d1ec9f35c8e2209e3457fad8a57423f524dc2a
SoftWareEngeneerDev <mailforlearning19@gmail.com>
2022-09-02 14:45:22 +0000

    fin de l'exercice5 Week2 Day1 Exercice XP

---
+++ week2/Day1/Exercices XP/Exercice5.js
@@ -0,0 +1,63 @@
+typeof(15)
+// Prediction:number
+// Actual: number
+
+typeof(5.5)
+// Prediction:number
+// Actual:number
+
+typeof(NaN)
+// Prediction:objet
+// Actual:number
+
+typeof("hello")
+// Prediction:String
+// Actual:string
+
+typeof(true)
+// Prediction:boolean
+// Actual:boolean
+
+typeof(1 != 2)
+// Prediction:boolean
+// Actual:boolean
+
+"hamburger" + "s"
+// Prediction:hamburges
+// Actual:hamburges
+
+"hamburgers" - "s"
+// Prediction:error
+// Actual: NaN
+
+"1" + "3"
+// Prediction:'13'
+// Actual:'13'
+
+"1" - "3"
+// Prediction: NaN
+// Actual:-2
+
+"johnny" + 5
+// Prediction:'Johnny5'
+// Actual:'Johnny5'
+
+    "johnny" - 5
+    // Prediction:NaN
+    // Actual:NaN
+
+99 * "hello"
+    // Prediction:NaN
+    // Actual:NaN
+
+1 != 1
+    // Prediction:false
+    // Actual:false
+
+1 == "1"
+    // Prediction:false
+    // Actual:true
+
+1 === "1"
+    // Prediction:false
+    // Actual:false
