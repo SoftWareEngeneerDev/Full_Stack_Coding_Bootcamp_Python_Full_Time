b273a0a7064fd01bd83eba04632d1aada5b9fd2c
SoftWareEngeneerDev <mailforlearning19@gmail.com>
2022-09-04 11:59:04 +0000

    fin de la premiere partie de l'exercice1 week2 Day3 ExerciceXP

---
+++ week2/Day3/ExercicesXP/Exercice1.js
@@ -0,0 +1,12 @@
+//--------------------------------------Partie I - Examen Des Tableaux----------------------------//
+//------------------------------------------------------------------------------------------------//
+let people = ["Greg", "Mary", "Devon", "James"];
+delete(people[0])
+people[3] = "Jason"
+people.push('KAGAMBEGA')
+console.log(people.indexOf('Mary'))
+let newPeople = people.slice(2, 5)
+console.log(newPeople)
+console.log(people.indexOf('Foo'))
+let last = people.pop();
+//-----------------------------------------END-----------------------------------------------------//
