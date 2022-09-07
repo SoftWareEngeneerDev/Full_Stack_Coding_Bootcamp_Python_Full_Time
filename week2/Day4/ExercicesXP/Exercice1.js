08000a271eb37a39d080f603536dfe394cb774b7
SoftWareEngeneerDev <mailforlearning19@gmail.com>
2022-09-07 00:43:58 +0000

    Fin de l'esxercice1 week2 Day4 ExerciceXP

---
+++ week2/Day4/ExercicesXP/Exercice1.js
@@ -0,0 +1,16 @@
+//----------------------------------Start partie 1---------------------------------------------
+function infoAboutMe() {
+    console.log("Mon  est KAGAMBEGA Aboubacar et mon sport de loisr est le Football")
+}
+infoAboutMe()
+    //----------------------------------endPartieI---------------------------------------------
+
+
+
+//---------------------------------------StartParti2-------------------------------------------
+
+function infoAboutPerson(personName, personAge, personFavoriteColor) {
+    console.log("Mon nom est " + " " + personName + "," + "mon age est" + " " + personAge + " " + "et ma couleur préféré est" + " " + personFavoriteColor)
+}
+infoAboutPerson("David", 45, "blue")
+infoAboutPerson("Josh", 12, "yellow")
