06b25890a932fc3a54c5571ecd5fd0d1fd6d4eb4
SoftWareEngeneerDev <mailforlearning19@gmail.com>
2022-09-07 10:15:39 +0000

    Fin de l'exercice1 week2 Day3 ExerciceXpGold

---
+++ week2/Day3/ExercicesXPGold/Exercice1.js
@@ -0,0 +1,10 @@
+/*
+-----------------------------Exercice 1 : Divisible Par Trois--------------------------------------------------------
+*/
+let numbers = [123, 8409, 100053, 333333333, 7];
+
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 3 == 0) {
+        console.log("true")
+    } else { console.log("false") }
+}
