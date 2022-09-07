0d565b2f2132ca49e91065c6a4df9c2ef246be0e
SoftWareEngeneerDev <mailforlearning19@gmail.com>
2022-09-07 12:30:13 +0000

    Fin de l'exercice9 week4 Python Day1 ExerciceXP

---
+++ week4/Day1/ExercicesXP/Exercice9.py
@@ -0,0 +1,6 @@
+taille = input("Enter your taille :")
+taille = float(taille)
+if taille >= 145:
+    print("Bravo !! vous pouvez rouler")
+else:
+    print("Attender un peu avant de pouvoir rouler")
