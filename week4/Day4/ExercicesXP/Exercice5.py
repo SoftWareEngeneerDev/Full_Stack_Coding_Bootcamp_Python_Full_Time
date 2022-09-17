cc918d37f6ae2142b17ec190f1a48dc6bb6c25e4
SoftWareEngeneerDev <mailforlearning19@gmail.com>
2022-09-17 14:25:28 +0000

    Fin de l'exercice5 week4 Day4 ExerciceXP

---
+++ week4/Day4/ExercicesXP/Exercice5.py
@@ -0,0 +1,15 @@
+def  make_shirt(size=50,string="J'aime Python"):
+    #size = len(string)
+    #print(type(string))
+    #print(type(size))
+    print("The size of the shirt is",size," and the text is",string)
+
+make_shirt(23,"un chemise")
+
+make_shirt()
+
+make_shirt(75)
+
+make_shirt(10)
+
+make_shirt(60,"Le coton est utilisé pour fabriqué des habits")
