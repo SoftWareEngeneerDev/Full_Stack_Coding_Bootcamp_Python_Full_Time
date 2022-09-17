709f7febf38fc8f8d74062c1f3ab12009ab7e54b
SoftWareEngeneerDev <mailforlearning19@gmail.com>
2022-09-17 14:24:01 +0000

    Fin de l'exercice1 week4 Day4 ExerciceXP

---
+++ week4/Day4/ExercicesXP/Exercice1.py
@@ -0,0 +1,9 @@
+#Écrivez une fonction appelée display_message()
+# qui imprime une phrase disant à tout le monde ce que vous apprenez dans ce cours.
+#Appelez la fonction et assurez-vous que le message s’affiche correctement.
+
+#
+def display_message():
+    print("Je suis le BootCamp de FullStack Python a Orange Digital Center ")
+
+display_message()
