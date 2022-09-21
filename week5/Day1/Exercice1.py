d2d3cc85374e3e65e9f4505626507f1d1a86b74a
SoftWareEngeneerDev <mailforlearning19@gmail.com>
2022-09-21 23:08:39 +0000

    mise à jour Exercice1 POO week5 ExercicexP Version1

--- week5/Day1/Exercice1.py
+++ week5/Day1/Exercice1.py
@@ -2,23 +2,18 @@ class Cat:
     def __init__(self, cat_name, cat_age):
         self.name = cat_name
         self.age = cat_age
-#chat1 = Cat("Bruno", 3)
-#print(chat1.name, chat1.age)
+
 def older():
     chat1 = Cat("Bruno", 3)
     chat2 = Cat("Miou",6)
     chat3 = Cat("brish",4)
+    age_chat=[chat1.age,chat2.age,chat3.age]
+    max_age = age_chat[0]
+    for i in range(0,len(age_chat)):
+        if age_chat[i] >max_age:
+            max_age = age_chat[i]
+    print("le plus agé a",max_age,"ans")
 
-    max = chat1.age
-    if max > chat1.age:
-        max = chat1.age
-        name = chat1.name
-    if max > chat2.age:
-        max = chat2.age
-        name = chat2.name
-    if max > chat3.age:
-        max = chat3.age
-        name = chat3.name
-   print(name)
 
-older()
+print(older())
+
