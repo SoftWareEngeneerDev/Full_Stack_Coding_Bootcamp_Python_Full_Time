db1384d66298582694b220867522c29e09b132ad
SoftWareEngeneerDev <mailforlearning19@gmail.com>
2022-09-24 18:56:44 +0000

    Fin de l'exercice2 ExerciceXPGold week5 Day1

--- week5/Day1/ExercicesXPGold/Exercice2.py
+++ week5/Day1/ExercicesXPGold/Exercice2.py
@@ -2,19 +2,27 @@ class MyList:
     def __init__(self,list):
         self.list = list
 
+    def affichage(self):
+        print(self.list)
+
     def listReverse(self):
-        return reversed(self.list)
+        self.list.reverse()
+
     def trie(self):
-        return self.list.sort()
+        self.list.sort()
 
-kaga = MyList(["k","a","g","a","m"])
+    def listAleatoir(self):
+        liste2 = []
+        for i in range(0,5):
+            liste2.append(i)
+        print(liste2)
 
 
-print(kaga)
-print(kaga.listReverse())
-print(kaga.trie())
 
-"""liste = ["k","a","g","a","m"]
-print(liste)
-rev = liste.reverse()
-print(rev)"""
+MonListe = MyList(["k","a","g","a","m"])
+MonListe.affichage()
+MonListe.listReverse()
+MonListe.affichage()
+MonListe.trie()
+MonListe.affichage()
+MonListe.listAleatoir()
