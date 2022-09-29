8ad06971f9f6d398dfafea0d63ec9771fe447325
SoftWareEngeneerDev <mailforlearning19@gmail.com>
2022-09-29 11:02:06 +0000

    Fin de l'exercice2 Day1 ExerciceXP week5

---
+++ week5/Day1/ExercicesXP/Exercice2.py
@@ -0,0 +1,24 @@
+class Dog:
+    def __init__(self, name, height):
+        self.name = name
+        self.height = height
+
+    def bark(self):
+        print("Le nom du chien est :",self.name)
+
+    def jump(self):
+        print(self.name," jumps ",self.height*2," cm de haut ! ».")
+        taille = self.height*2
+
+davids_dog = Dog("Rex",50)
+print(davids_dog.bark())
+print(davids_dog.jump())
+
+sarahs_dog = Dog("Teacup",20)
+print(sarahs_dog.bark())
+print(sarahs_dog.jump())
+#fin de l'exercice
+"""if taille1> taille2:
+    print(davids_dog.bark(),"est le plus grand et sa taille est :", davids_dog.jump())
+else:
+    print(sarahs_dog.bark(),"est le plus grand et sa taille est :", sarahs_dog.jump())"""
