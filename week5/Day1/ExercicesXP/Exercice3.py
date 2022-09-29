5a7ab8235b93078bed2c52609e635c6c3e5d3df8
SoftWareEngeneerDev <mailforlearning19@gmail.com>
2022-09-29 11:02:19 +0000

    Fin de l'exercice3 Day1 ExerciceXP week5

---
+++ week5/Day1/ExercicesXP/Exercice3.py
@@ -0,0 +1,13 @@
+class  Song:
+    #lyrics = ["Jazz","Raggae","Hip Hop"]
+    def __init__(self, lyrics):
+        self.lyrics = lyrics
+
+    def sing_me_a_song(self):
+        for i in range(0,len(self.lyrics)):
+            print(self.lyrics[i])
+#Fin de la fonction
+
+stairway= Song(["There’s a lady who's sure","all that glitters is gold", "and she’s buying a stairway to heaven"])
+
+print(stairway.sing_me_a_song())
