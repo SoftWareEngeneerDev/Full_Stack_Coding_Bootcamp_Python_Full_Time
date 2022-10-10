4b612dd2c3c19f7a70f15f5bad8587efb4fa94af
SoftWareEngeneerDev <mailforlearning19@gmail.com>
2022-10-10 15:07:48 +0000

    Fin exercice DailyChalenge

--- week6/DailyChallenge/Exercice.sql
+++ week6/DailyChallenge/Exercice.sql
@@ -22,6 +22,7 @@
 		 number_oscars SMALLINT NOT NULL
 	);*/
 
+--Insertion
 	
 INSERT INTO actors (first_name, last_name, age, number_oscars) VALUES('Matt','Damon','08/10/1970', 5);
 INSERT INTO actors (first_name, last_name, age, number_oscars) VALUES('George','Clooney','06/05/1961', 2);
